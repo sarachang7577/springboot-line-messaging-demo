ac94e8a77d57c36a5ca7ca7f42253e16ae38b816
sarachang757 <sarachang7577@gmail.com>
2022-10-17 14:33:18 +0800

    setup a Sprint Boot project

---
+++ src/main/java/com/example/springbootlinemessagingdemo/SpringbootLineMessagingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlinemessagingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLineMessagingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLineMessagingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootlinemessagingdemo/SpringbootLineMessagingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlinemessagingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLineMessagingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
